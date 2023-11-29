9e906cee0e53fad9c6ebbc08a77cd9062407ea40
Giovanni <gioabrami@gmail.com>
2023-11-29 19:42:19 -0300

    Teste da pagina do cardápio

---
+++ js/funcoes.js
@@ -0,0 +1,13 @@
+function produto(nome, descricao, preco) {
+    document.write(`
+    <div>    
+    <img src="imagens/burguer.png" width="30px">
+    <br>
+    <h4>Nome: ${nome}</h4>
+    <br>
+    <p>Descrição: ${descricao}</p>
+    <br>
+    <p>Preço: ${preco}</p>
+        
+        `)
+}
