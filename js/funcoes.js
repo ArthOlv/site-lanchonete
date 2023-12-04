0874d62b8a8e7d56b54e717a354c633245f859fb
Arth <arthur99810@gmail.com>
2023-12-04 18:02:33 -0300

    commit 4 - pagina cardapio 80% finalizada, margin arrumada

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,33 +1,8 @@
 function produto(nome, descricao, preco, imagem) {
     document.write(`
-    <style>
-        .linha{
-            display: flex;
-            flex-flow: row wrap;
-            justify-content: center;
-            }
-        .coluna-1{
-            width: 15%;
-            margin-top: auto;
-            margin-bottom: auto;
-        }
-        .coluna-2{
-            width: 40%
-        }
-        .coluna-3{
-            width: 20%
-        }
-        #descricao{
-            color: burlywood;
-        }
-        .texto-produtos{
-            color: #FFF;
-        }
-    </style>
     <div class="linha">
-        <div class="coluna-1">
-            <br>  
-            <img src="${imagem}" width="80px">
+        <div class="coluna-1"> 
+            <img src="${imagem}" width="160px">
         </div>
         <div class="coluna-2">
             <h4 class="texto-produtos">${nome}</h4>
@@ -35,8 +10,8 @@ function produto(nome, descricao, preco, imagem) {
             <p class="texto-produtos">R$ ${preco}</p>
         </div>
         <div class="coluna-3">
-            <br>
-            <input type="number" id="${nome}">
+            <input type="number" id="${nome}" min="0" step="0">
+            <p id='unidade'>unidade(s)</p>
         </div>
     </div>
         `);
